e85b4cc86847877a67ac53053c55c7656ae0c4c5
Yaroslav <yaroslavlit@gmail.com>
2025-09-06 18:33:01 +0200

    fix problems v 1.6

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -13,10 +13,12 @@ interface Props {
 
 export default async function Notes({ params }: Props) {
   const { slug } = await params;
-  const tag =
-    slug?.[0] && slug[0] !== "all"
-      ? ((slug[0].charAt(0).toUpperCase() + slug[0].slice(1)) as Note["tag"])
-      : undefined;
+  // const tag =
+  //   slug?.[0] && slug[0] !== "all"
+  //     ? ((slug[0].charAt(0).toUpperCase() + slug[0].slice(1)) as Note["tag"])
+  //     : undefined;
+
+  const tag = slug[0] === "All" ? undefined : (slug[0] as Note["tag"]);
 
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
